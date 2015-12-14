8d780c075da3fe9eda0b1b98557d165872824d62
ribsousa <ribbsousa@gmail.com>
2015-12-14 18:02:20 -0200

    Update Week.php

--- components/Week.php
+++ components/Week.php
@@ -18,47 +18,47 @@ class Week extends ComponentBase {
 
 	public function componentDetails() {
 		return [
-			'name' => 'Week Component',
-			'description' => 'Shows a week calendar with events',
+			'name' => 'kurtjensen.mycalendar::lang.week.name',
+			'description' => 'kurtjensen.mycalendar::lang.week.description',
 		];
 	}
 
 	public function defineProperties() {
 		return [
 			'month' => [
-				'title' => 'Month',
-				'description' => 'The month you want to show.',
+				'title' => 'kurtjensen.mycalendar::lang.month.month_title',
+				'description' => 'kurtjensen.mycalendar::lang.month.month_description',
 			],
 			'year' => [
-				'title' => 'Year',
-				'description' => 'The year you want to show.',
+				'title' => 'kurtjensen.mycalendar::lang.month.year_title',
+				'description' => 'kurtjensen.mycalendar::lang.month.year_description',
 			],
 			'events' => [
-				'title' => 'Events',
-				'description' => 'Array of the events you want to show.',
+				'title' => 'kurtjensen.mycalendar::lang.month.events_title',
+				'description' => 'kurtjensen.mycalendar::lang.month.events_description',
 			],
 			'color' => [
-				'title' => 'Calendar Color',
-				'description' => 'Array of the events you want to show.',
+				'title' => 'kurtjensen.mycalendar::lang.month.color_title',
+				'description' => 'kurtjensen.mycalendar::lang.month.color_description',
 				'type' => 'dropdown',
-				'default' => 'red',
+				'default' => 'kurtjensen.mycalendar::lang.evlist.color_default',
 			],
 			'dayprops' => [
-				'title' => 'Day Properties',
-				'description' => 'Array of the properties you want to put on the day indicator.',
+				'title' => 'kurtjensen.mycalendar::lang.month.dayprops_title',
+				'description' => 'kurtjensen.mycalendar::lang.month.dayprops_description',
 			],
 			'loadstyle' => [
-				'title' => 'Load Style Sheet',
-				'description' => 'Load the default CSS file.',
+				'title' => 'kurtjensen.mycalendar::lang.month.loadstyle_title',
+				'description' => 'kurtjensen.mycalendar::lang.month.loadstyle_description',
 				'type' => 'dropdown',
 				'default' => '1',
-				'options' => [0 => 'No', 1 => 'Yes'],
+				'options' => [0 => 'kurtjensen.mycalendar::lang.month.opt_no', 1 => 'kurtjensen.mycalendar::lang.month.opt_yes'],
 			],
 		];
 	}
 
 	public function getColorOptions() {
-		return ['red' => 'red', 'green' => 'green', 'blue' => 'blue', 'yellow' => 'yellow'];
+		return ['red' => 'kurtjensen.mycalendar::lang.evlist.color_red', 'green' => 'kurtjensen.mycalendar::lang.evlist.color_green', 'blue' => 'kurtjensen.mycalendar::lang.evlist.color_blue', 'yellow' => 'kurtjensen.mycalendar::lang.evlist.color_yellow'];
 	}
 
 	public function onRender() {
@@ -76,7 +76,7 @@ class Week extends ComponentBase {
 
 	public function calcElements() {
 
-		$this->calHeadings = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+		$this->calHeadings = ['kurtjensen.mycalendar::lang.evlist.day_sun', 'kurtjensen.mycalendar::lang.evlist.day_mon', 'kurtjensen.mycalendar::lang.evlist.day_tue', 'kurtjensen.mycalendar::lang.evlist.day_wed', 'kurtjensen.mycalendar::lang.evlist.day_thu', 'kurtjensen.mycalendar::lang.evlist.day_fri', 'kurtjensen.mycalendar::lang.evlist.day_sat'];
 		$time = strtotime($this->month . '/1/' . $this->year);
 		$this->monthTitle = date('F', $time);
 		$this->monthNum = date('n', $time);
